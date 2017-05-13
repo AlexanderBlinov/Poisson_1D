767c3f9109b1b03b90b5157ca1c8f30824a4ae0c
Alexander Blinov <alexanderblinov@Alexanders-MacBook-Pro.local>
2017-05-13 21:22:39 +0300

    Reduced amount of used memory.

--- main.cpp
+++ main.cpp
@@ -54,14 +54,14 @@ int main(int argc, char* argv[]) {
     start = MPI_Wtime();
 
     // Vichislenie osnovnih parametrov oblasti reshenija
-    int rit = 200, tag = 1000;
+    int rit = 100, tag = 1000;
     double h1 = 0.01, h2 = 0.01, h3 = 0.01;
     double X = 1, Y = 1, Z = 1;
-    double w = 1;
+    double w = 1.7;
 
     int Nx = (int)(X / h1) - 1, Ny = (int)(Y / h2) - 1, Nz = (int)(Z / h3) - 1;
 
-    int r1 = 20, r3 = 20, r4 = 20;
+    int r1 = 10, r3 = 40, r4 = 10;
     int Q1 = (int)ceil((double)rit / r1);
     int Q3 = (int)ceil((double)(rit + Ny) / r3);
     int Q4 = (int)ceil((double)(rit + Nz) / r4);
@@ -69,8 +69,10 @@ int main(int argc, char* argv[]) {
     int Q2 = size;
     int r2 = (int)ceil((double)(rit + Nx) / Q2);
 
-    double *U = (double *)calloc((size_t)(r1 * Q1 + 1) * r2 * r3 * Q3 * r4 * Q4, sizeof(double));
-    double *preLeft = (double *)calloc((size_t)(r1 * Q1 + 1) * r3 * Q3 * r4 * Q4, sizeof(double));
+    double *U = (double *)calloc((size_t)(r1 + 1) * r2 * r3 * Q3 * r4 * Q4, sizeof(double));
+    double *buffU = (double *)calloc((size_t)r1 * r2 * r3 * r4, sizeof(double));
+    double *preLeft = (double *)calloc((size_t)(r1 + 1) * r3 * Q3 * r4 * Q4, sizeof(double));
+    double *buffPreLeft = (double *)calloc((size_t)r1 * r3 * r4, sizeof(double));
 
     int igl2 = rank;
     int left = rank - 1;
@@ -80,9 +82,7 @@ int main(int argc, char* argv[]) {
     MPI_Request request_left = MPI_REQUEST_NULL, request_right = MPI_REQUEST_NULL;
 
     MPI_Datatype ujk_t;
-    MPI_Type_vector(r3, r4, r4 * Q4, MPI_DOUBLE, &ujk_t);
-    MPI_Type_commit(&ujk_t);
-    MPI_Type_create_resized(ujk_t, 0, sizeof(double) * r2 * r3 * Q3 * r4 * Q4, &ujk_t);
+    MPI_Type_vector(r1, r3 * r4, r2 * r3 * r4, MPI_DOUBLE, &ujk_t);
     MPI_Type_commit(&ujk_t);
 
     MPI_Datatype  prejk_t;
@@ -120,18 +120,18 @@ int main(int argc, char* argv[]) {
 
                 if (left != -1) {
                     if (!leftRecved) {
-                        MPI_Recv(preLeft + ((igl1 * r1 + 1) * Q3 + igl3) * r3 * r4 * Q4 + igl4 * r4, r1, prejk_t,
+                        MPI_Recv(preLeft + ((Q3 + igl3) * r3 * Q4 + igl4) * r4, r1, prejk_t,
                                  left, tag * igl1 + igl4, MPI_COMM_WORLD, &status);
                         leftRecved = true;
                     }
                     if (next) {
-                        MPI_Irecv(preLeft + ((igl1_n * r1 + 1) * Q3 + igl3_n) * r3 * r4 * Q4 + igl4_n * r4, r1, prejk_t,
+                        MPI_Irecv(buffPreLeft, r1 * r3 * r4, MPI_DOUBLE,
                                   left, tag * igl1_n + igl4_n, MPI_COMM_WORLD, &request_left);
                     }
                 }
 
 //                printf("%d enters tile %d %d %d\n", rank, igl1, igl3, igl4);
-                for (int i1 = igl1 * r1 + 1; i1 < min((igl1 + 1) * r1 + 1, rit + 1); ++i1) {
+                for (int i1 = igl1 * r1 + 1, ii1 = 1; i1 < min((igl1 + 1) * r1 + 1, rit + 1); ++i1, ++ii1) {
                     for (int i2 = max(0, i1 - igl2 * r2); i2 < min(r2, i1 + Nx - igl2 * r2); ++i2) {
                         for (int i3 = max(igl3 * r3, i1); i3 < min((igl3 + 1) * r3, i1 + Ny); ++i3) {
                             for (int i4 = max(igl4 * r4, i1); i4 < min((igl4 + 1) * r4, i1 + Nz); ++i4) {
@@ -142,53 +142,53 @@ int main(int argc, char* argv[]) {
                                 if (i == 0) {
                                     uim = A0((j + 1) * h2, (k + 1) * h3);
                                 } else if (i2 == 0) {
-                                    uim = preLeft[(i1 * r3 * Q3 + i3) * r4 * Q4 + i4];
+                                    uim = preLeft[(ii1 * r3 * Q3 + i3) * r4 * Q4 + i4];
                                 } else {
-                                    uim = U[((i1 * r2 + i2 - 1) * r3 * Q3 + i3) * r4 * Q4 + i4];
+                                    uim = U[((ii1 * r2 + i2 - 1) * r3 * Q3 + i3) * r4 * Q4 + i4];
                                 }
 
                                 if (i == Nx - 1) {
                                     uip = A1((j + 1) * h2, (k + 1) * h3, X);
                                 } else {
-                                    uip = U[(((i1 - 1) * r2 + i2) * r3 * Q3 + i3 - 1) * r4 * Q4 + i4 - 1];
+                                    uip = U[(((ii1 - 1) * r2 + i2) * r3 * Q3 + i3 - 1) * r4 * Q4 + i4 - 1];
                                 }
 
 
                                 if (j == 0) {
                                     ujm = B0((i + 1) * h1, (k + 1) * h3);
                                 } else {
-                                    ujm = U[((i1 * r2 + i2) * r3 * Q3 + i3 - 1) * r4 * Q4 + i4];
+                                    ujm = U[((ii1 * r2 + i2) * r3 * Q3 + i3 - 1) * r4 * Q4 + i4];
                                 }
 
                                 if (j == Ny - 1) {
                                     ujp = B1((i + 1) * h1, (k + 1) * h3, Y);
                                 } else if (i2 == 0) {
-                                    ujp = preLeft[((i1 - 1) * r3 * Q3 + i3) * r4 * Q4 + i4 - 1];
+                                    ujp = preLeft[((ii1 - 1) * r3 * Q3 + i3) * r4 * Q4 + i4 - 1];
                                 } else {
-                                    ujp = U[(((i1 - 1) * r2 + i2 - 1) * r3 * Q3 + i3) * r4 * Q4 + i4 - 1];
+                                    ujp = U[(((ii1 - 1) * r2 + i2 - 1) * r3 * Q3 + i3) * r4 * Q4 + i4 - 1];
                                 }
 
                                 if (k == 0) {
                                     ukm = C0((i + 1) * h1, (j + 1) * h2);
                                 } else {
-                                    ukm = U[((i1 * r2 + i2) * r3 * Q3 + i3) * r4 * Q4 + i4 - 1];
+                                    ukm = U[((ii1 * r2 + i2) * r3 * Q3 + i3) * r4 * Q4 + i4 - 1];
                                 }
 
                                 if (k == Nz - 1) {
                                     ukp = C1((i + 1) * h1, (j + 1) * h2, Z);
                                 } else if (i2 == 0) {
-                                    ukp = preLeft[((i1 - 1) * r3 * Q3 + i3 - 1) * r4 * Q4 + i4];
+                                    ukp = preLeft[((ii1 - 1) * r3 * Q3 + i3 - 1) * r4 * Q4 + i4];
                                 } else {
-                                    ukp = U[(((i1 - 1) * r2 + i2 - 1) * r3 * Q3 + i3 - 1) * r4 * Q4 + i4];
+                                    ukp = U[(((ii1 - 1) * r2 + i2 - 1) * r3 * Q3 + i3 - 1) * r4 * Q4 + i4];
                                 }
 
                                 if (i2 == 0) {
-                                    u = preLeft[((i1 - 1) * r3 * Q3 + i3 - 1) * r4 * Q4 + i4 - 1];
+                                    u = preLeft[((ii1 - 1) * r3 * Q3 + i3 - 1) * r4 * Q4 + i4 - 1];
                                 } else {
-                                    u = U[(((i1 - 1) * r2 + i2 - 1) * r3 * Q3 + i3 - 1) * r4 * Q4 + i4 - 1];
+                                    u = U[(((ii1 - 1) * r2 + i2 - 1) * r3 * Q3 + i3 - 1) * r4 * Q4 + i4 - 1];
                                 }
 
-                                U[((i1 * r2 + i2) * r3 * Q3 + i3) * r4 * Q4 + i4] =
+                                U[((ii1 * r2 + i2) * r3 * Q3 + i3) * r4 * Q4 + i4] =
                                         w * ((uip + uim) / (h1 * h1)
                                              + (ujp + ujm) / (h2 * h2)
                                              + (ukp + ukm) / (h3 * h3) - F((i + 1) * h1,
@@ -202,15 +202,46 @@ int main(int argc, char* argv[]) {
 //                printf("%d leaves tile %d %d %d\n", rank, igl1, igl3, igl4);
 
                 if (right != -1) {
-                    if (request_right != MPI_REQUEST_NULL) {
-                        MPI_Wait(&request_right, &status);
+                    MPI_Wait(&request_right, &status);
+
+                    for (int l = 0; l < r1; ++l) {
+                        for (int i = 0; i < r2; ++i) {
+                            for (int j = 0; j < r3; ++j) {
+                                for (int k = 0; k < r4; ++k) {
+                                    buffU[((l * r2 + i) * r3 + j) * r4 + k]
+                                            = U[(((((l + 1) * r2 + i) * Q3 + igl3) * r3 + j) * Q4 + igl4) * r4 + k];
+                                }
+                            }
+                        }
                     }
-                    MPI_Isend(U + (((igl1 * r1 + 2) * r2 - 1) * Q3 + igl3) * r3 * r4 * Q4 + igl4 * r4, r1, ujk_t,
+
+                    MPI_Isend(buffU + (r2 - 1) * r3 * r4, 1, ujk_t,
                               right, tag * igl1 + igl4, MPI_COMM_WORLD, &request_right);
                 }
 
                 if (left != -1 && next) {
                     MPI_Wait(&request_left, &status);
+                    for (int l = 0; l < r1; ++l) {
+                        for (int j = 0; j < r3; ++j) {
+                            for (int k = 0; k < r4; ++k) {
+                                preLeft[((((l + 1) * Q3 + igl3) * r3 + j) * Q4 + igl4_n) * r4 + k]
+                                        = buffPreLeft[(l * r3 + j) * r4 + k];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if (igl1 != Q1 - 1) {
+            for (int i = 0; i < r2; ++i) {
+                for (int j = 0; j < r3 * Q3; ++j) {
+                    for (int k = 0; k < r4 * Q4; ++k) {
+                        U[(i * r3 * Q3 + j) * r4 * Q4 + k] = U[((r1 * r2 + i) * r3 * Q3 + j) * r4 * Q4 + k];
+                        if (i == 0) {
+                            preLeft[j * r4 * Q4 + k] = preLeft[(r1 * r3 * Q3 + j) * r4 * Q4 + k];
+                        }
+                    }
                 }
             }
         }
@@ -223,7 +254,12 @@ int main(int argc, char* argv[]) {
         R = (double *) malloc(sizeof(double) * r2 * Q2 * r3 * Q3 * r4 * Q4);
     }
 
-    MPI_Gather(U + rit * r2 * r3 * Q3 * r4 * Q4, r2 * r3 * Q3 * r4 * Q4, MPI_DOUBLE,
+    int l = rit % r1;
+    if (l == 0) {
+        l = r1;
+    }
+
+    MPI_Gather(U + l * r2 * r3 * Q3 * r4 * Q4, r2 * r3 * Q3 * r4 * Q4, MPI_DOUBLE,
                R, r2 * r3 * Q3 * r4 * Q4, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     MPI_Barrier(MPI_COMM_WORLD);
